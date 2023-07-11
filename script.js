da9adec33252f2db21f0763f61a1ce63dad4000e
Aswin <115768572+aswin-02@users.noreply.github.com>
2023-07-11 09:52:05 +0530

    Add files via upload
    a portfolio, what else it could be.........

---
+++ script.js
@@ -0,0 +1,9 @@
+function linkedin(){
+    window.open("https://www.linkedin.com/in/aswin05/");
+}
+function git(){
+    window.open("https://github.com/aswin-02");
+}
+function mail(){
+    window.open("mailto:aswinvijay325@gmail.com?subject = Feedback&body = Message");
+}
